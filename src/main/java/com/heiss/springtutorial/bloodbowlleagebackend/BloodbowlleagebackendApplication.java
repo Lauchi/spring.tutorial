f3cb33fd8cf0fc4dbf6f928f2e37ce91e45aa856
Simon Heiss <simonheiss87@gmail.com>
2019-09-10 19:28:22 +0200

    Initial commit

---
+++ src/main/java/com/heiss/springtutorial/bloodbowlleagebackend/BloodbowlleagebackendApplication.java
@@ -0,0 +1,13 @@
+package com.heiss.springtutorial.bloodbowlleagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodbowlleagebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodbowlleagebackendApplication.class, args);
+    }
+
+}
