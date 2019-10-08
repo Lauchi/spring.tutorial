d693e6f80782068ec36887b0f5949a21f551f9ce
Simon Heiss <simonheiss87@gmail.com>
2019-10-08 17:58:58 +0200

    added data.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,28 +6,6 @@ CREATE TABLE Ingredient
   primary key (id)
 );
 
-delete from Ingredient;
-insert into Ingredient (id, name, ingredientType)
-values ('FLTO', 'Flour Tortilla', 'WRAP');
-insert into Ingredient (id, name, ingredientType)
-values ('COTO', 'Corn Tortilla', 'WRAP');
-insert into Ingredient (id, name, ingredientType)
-values ('GRBF', 'Ground Beef', 'PROTEIN');
-insert into Ingredient (id, name, ingredientType)
-values ('CARN', 'Carnitas', 'PROTEIN');
-insert into Ingredient (id, name, ingredientType)
-values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
-insert into Ingredient (id, name, ingredientType)
-values ('LETC', 'Lettuce', 'VEGGIES');
-insert into Ingredient (id, name, ingredientType)
-values ('CHED', 'Cheddar', 'CHEESE');
-insert into Ingredient (id, name, ingredientType)
-values ('JACK', 'Monterrey Jack', 'CHEESE');
-insert into Ingredient (id, name, ingredientType)
-values ('SLSA', 'Salsa', 'SAUCE');
-insert into Ingredient (id, name, ingredientType)
-values ('SRCR', 'Sour Cream', 'SAUCE');
-
 CREATE TABLE Taco(
    id varchar(255) not null,
    tacoName varchar(255) not null,
