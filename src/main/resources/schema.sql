fd6e5a52ceee6c4730e906330db5760f2190bb13
Simon Heiss <simonheiss87@gmail.com>
2019-10-10 09:26:00 +0200

    use identity instead of int

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,40 +1,35 @@
 CREATE TABLE Ingredient
 (
-  id varchar(4) not null,
-  name varchar(255) not null,
-  ingredientType varchar(255) not null,
-  primary key (id)
-);
-
-CREATE TABLE Taco(
-    id bigint not null auto_increment,
-    tacoName varchar(255) not null,
+    id varchar(4) not null,
+    name varchar(255) not null,
+    ingredientType varchar(255) not null,
     primary key (id)
 );
 
+CREATE TABLE Taco(
+    id identity,
+    tacoName varchar(255) not null
+);
+
 CREATE TABLE TacoIngredientsCrossMap(
- id bigint not null auto_increment,
- tacoId varchar(255) not null,
- ingredientId varchar(4) not null,
+    id identity,
+    tacoId varchar(255) not null,
+    ingredientId varchar(4) not null,
 
- primary key (id),
-
- foreign key (tacoId) references Taco(id),
- foreign key (ingredientId) references Ingredient (id)
+    foreign key (tacoId) references Taco(id),
+    foreign key (ingredientId) references Ingredient (id)
 );
 
 CREATE TABLE TacoOrder(
-  id bigint not null auto_increment,
-  tacoId bigint not null,
-  placedAt datetime not null,
+    id identity,
+    tacoId bigint not null,
+    placedAt datetime not null,
 
-  name varchar(255) not null,
-  street varchar(255) not null,
-  city varchar(255) not null,
-  state varchar(255) not null,
-  ccNumber varchar(255) not null,
+    name varchar(255) not null,
+    street varchar(255) not null,
+    city varchar(255) not null,
+    state varchar(255) not null,
+    ccNumber varchar(255) not null,
 
-  primary key (id),
-
-  foreign key (tacoId) references Taco(id)
+    foreign key (tacoId) references Taco(id)
 );
